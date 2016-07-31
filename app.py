b90e724b01fcf9ec66c460fffbbf1aba4ddf87e6
Pine Mizune <pinemz@gmail.com>
2016-07-31 23:11:28 +0900

    feat(icon): add favicon

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.j2')
+
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
