54f8c89e038a34d694f27ef95231350195e443f0
Pine Mizune <pinemz@gmail.com>
2016-08-04 12:25:51 +0900

    feat(parts): add parts directory

---
+++ view/parts/footer/index.js
@@ -0,0 +1,6 @@
+import './index.css'
+
+module.exports = {
+  name: 'eg-footer',
+  template: require('./index.html'),
+}
---
+++ view/parts/header/index.js
@@ -0,0 +1,6 @@
+import './index.css'
+
+module.exports = {
+  name: 'eg-header',
+  template: require('./index.html'),
+}
