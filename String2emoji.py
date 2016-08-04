c944afd85e744e3270861e6223e9da831469d67a
jiuya <tatsuya.yamamoto.kbhit@gmail.com>
2016-08-04 15:13:13 +0900

    feat(app.py): add generate emoji function

---
+++ String2emoji.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from PIL import Image, ImageDraw, ImageFont
+
+class String2emoji(object):
+    """docstring for String2emoji"""
+    def __init__(self, argText,argFontName,argFontColor = (0,0,0,0)):
+        self.textList = argText
+        self.fontName = argFontName
+        self.backColor = (255,255,255,0)
+        self.imageSize = (128,128)
+        self.fontColor = argFontColor
+
+
+    def getFont(self,size):
+        return ImageFont.truetype(self.fontName, size, encoding='utf-8')
+
+    def setBackColor(self,color):
+        self.backColor = color
+
+    def setFontColor(self,color):
+        self.fontColor = color
+    def cutEffectiveRange(self,text,wMax,hMax):
+        for i in range(hMax,hMax*2):
+            font = self.getFont(i)
+            w, h = font.getsize(text)
+            img = Image.new("RGBA",(w,h),self.backColor)
+            draw = ImageDraw.Draw(img)
+            draw.text((0,0), text, fill=self.fontColor, font=font)
+            x0 = self.stringOverBorderX(img,text,font,h)
+            y0 = self.stringOverBorderY(img,text,font,w)
+            x1 = self.stringUnderBorderX(img,text,font,w,h) + x0
+            y1 = self.stringUnderBorderY(img,text,font,h,w) + y0
+            if ((x1 >= wMax-5) and (x1 < wMax)) or ((y1 >= hMax-5) and (y1 < hMax)) :
+                return (i,x0,y0,x1,y1);
+    def stringOverBorderX(self,img,text,font,h):
+        for x in range(0,-127,-1):
+            limitFlag = 0
+            for i in range(0,h-1):
+                color = img.getpixel((-x,i))
+                if color != self.backColor:
+                    limitFlag = 1;
+            if limitFlag > 0:
+                return x
+    def stringOverBorderY(self,img,text,font,w):
+        for y in range(0,-127,-1):
+            limitFlag = 0
+            for i in range(0,w-1,2):
+                color = img.getpixel((i,-y))
+                if color != self.backColor:
+                    limitFlag = 1;
+            if limitFlag > 0:
+                return y
+    def stringUnderBorderX(self,img,text,font,w,h):
+        for cx in range(w-1,0,-2):
+            for cy in range(0,h-1):
+                color = img.getpixel((cx,cy))
+                if color != self.backColor:
+                    return cx;
+    def stringUnderBorderY(self,img,text,font,h,w):
+        for cy in range(h-1,0,-1):
+            for cx in range(0,w-1,2):
+                color = img.getpixel((cx,cy))
+                if color != self.backColor:
+                    return cy;
+
+    def getEmoji(self):
+        img = Image.new("RGBA",self.imageSize,self.backColor)
+        draw = ImageDraw.Draw(img)
+        l = len(self.textList)
+
+        for i in range(0,l):
+            img_str = Image.new("RGBA",(len(self.textList[i])*128,128),self.backColor)
+            draw = ImageDraw.Draw(img_str)
+            (size,x0,y0,x1,y1) = self.cutEffectiveRange(self.textList[i],len(self.textList[i])*64,int(128/l))
+            #(size,x0,y0,x1,y1) = self.cutEffectiveRange(self.textList[i],256,128/l)
+            font = self.getFont(size)
+            draw.text((x0,y0), self.textList[i], fill=self.fontColor, font=font)
+            img_str.crop((0,0,x1,y1))
+            if x1 > 128:
+                img_str = img_str.transform(img_str.size,Image.AFFINE,(x1/128.0,0,0,0,1,0),Image.BICUBIC)
+                image_paste_x = 0
+            else:
+                #image_paste_x = int((128-x1)/2)
+                image_paste_x = 0
+            if l != 1:
+                img.paste(img_str,(image_paste_x,int((128/l)*i)))
+            else:
+                img.paste(img_str,(image_paste_x,int((128-y1)/2)))
+        return img
--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 from flask import Flask, render_template
+from flask import request, redirect, url_for
+from flask import Response,make_response
+from String2emoji import String2emoji
+import io
 
 app = Flask(__name__)
 
@@ -7,7 +11,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.j2')
 
-
+@app.route('/emoji')
+def emoji():
+    font = request.args.get("font", default='NotoSansCJKjp-hinted/NotoSansMonoCJKjp-Bold.otf', type=str)
+    text = request.args.get("text", default='test', type=str)
+    color = request.args.get("color", default='000000', type=str)
+    if font is False:
+        font = 'NotoSansCJKjp-hinted/NotoSansMonoCJKjp-Bold.otf'
+    if text is False:
+        text = ' '
+    if color is False:
+        color = '000000'
+    r = int(color[0] +color[1],16)
+    g = int(color[2] +color[3],16)
+    b = int(color[4] +color[5],16)
+    emoji = String2emoji(text.splitlines(), font,(r,g,b))
+    img = emoji.getEmoji()
+    output = io.BytesIO()
+    img.save(output,format='png')
+    res = make_response()
+    res.data = output.getvalue()
+    res.headers['Content-Type'] = 'image/png'
+    return res
 if __name__ == '__main__':
     port = os.getenv('PORT', '5000')
     app.debug = True
