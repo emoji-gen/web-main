20beaa69a40619853e055aed35c09e3b06c0be61
Pine Mizune <pinemz@gmail.com>
2016-08-07 23:31:23 +0900

    fix(scripts): start <-> end :innocent:

--- scripts/notify/start-deploy.sh
+++ scripts/notify/start-deploy.sh
@@ -5,7 +5,7 @@ set -e
 curl -XPOST \
     -d "token=$SLACK_API_TOKEN" \
     -d "channel=emoji" \
-    -d "text=end     deploy \`Emoji-Web\` *$CIRCLE_BRANCH* by CircleCI" \
+    -d "text=start   deploy \`Emoji-Web\` *$CIRCLE_BRANCH* by CircleCI" \
     -d "username=CircleCI" \
     -d "icon_emoji=:rocket:" \
     "https://slack.com/api/chat.postMessage"
