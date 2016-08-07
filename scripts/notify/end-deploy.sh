227001e8f16dc8d0f49fcc683a50372865c81f07
Pine Mizune <pinemz@gmail.com>
2016-08-07 23:44:54 +0900

    fix(scripts): change username

--- scripts/notify/end-deploy.sh
+++ scripts/notify/end-deploy.sh
@@ -6,6 +6,6 @@ curl -XPOST \
     -d "token=$SLACK_API_TOKEN" \
     -d "channel=emoji" \
     -d "text=end     deploy \`Emoji-Web\` *$CIRCLE_BRANCH* by CircleCI" \
-    -d "username=CircleCI" \
+    -d "username=Fabric" \
     -d "icon_emoji=:rocket:" \
     "https://slack.com/api/chat.postMessage"
