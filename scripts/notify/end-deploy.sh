b039a39a11a2a1f93b0ab6f9bb85b4480f1fba5a
Pine Mizune <pinemz@gmail.com>
2016-08-07 23:50:20 +0900

    fix(scripts): supress stdout and stderr

--- scripts/notify/end-deploy.sh
+++ scripts/notify/end-deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-set -e
-
 curl -XPOST \
     -d "token=$SLACK_API_TOKEN" \
     -d "channel=emoji" \
     -d "text=end     deploy \`Emoji-Web\` *$CIRCLE_BRANCH* by CircleCI" \
     -d "username=Fabric" \
     -d "icon_emoji=:rocket:" \
-    "https://slack.com/api/chat.postMessage"
+    "https://slack.com/api/chat.postMessage" > /dev/null 2>&1
--- scripts/notify/start-test.sh
+++ scripts/notify/start-test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 ICON_URL="https://dl.dropboxusercontent.com/u/29279948/Slack/Icons/CircleCI.png"
 
 curl -XPOST \
@@ -10,4 +8,4 @@ curl -XPOST \
     -d "text=start   test   \`Emoji-Web\` *$CIRCLE_BRANCH* by CircleCI" \
     -d "username=CircleCI" \
     -d "icon_url=$ICON_URL" \
-    "https://slack.com/api/chat.postMessage"
+    "https://slack.com/api/chat.postMessage" > /dev/null 2>&1
