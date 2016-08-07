8699c53e42226b985f552cbc26bd90c6e18bee00
Pine Mizune <pinemz@gmail.com>
2016-08-08 02:43:27 +0900

    fix(scripts/deploy.sh): fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ set -eu
 git clone --depth 1 -b master --single-branch git@github.com:pine/Emoji-Private.git
 cd Emoji-Private/fabric
 
-pip install fabric
+pip install -r requirements.txt -q
 fab production
