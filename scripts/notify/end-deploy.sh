c0a8f47017f8b626be528f505a7a8ade048e40fa
Pine Mizune <pinemz@gmail.com>
2016-08-07 23:29:36 +0900

    feat(ci): add notify scripts

---
+++ scripts/notify/end-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+curl -XPOST \
+    -d "token=$SLACK_API_TOKEN" \
+    -d "channel=emoji" \
+    -d "text=start   deploy \`Emoji-Web\` *$CIRCLE_BRANCH* by CircleCI" \
+    -d "username=CircleCI" \
+    -d "icon_emoji=:rocket:" \
+    "https://slack.com/api/chat.postMessage"
