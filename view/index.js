fac28d93db71ceb779b7efbd1aef3bdf335dd616
Pine Mizune <pinemz@gmail.com>
2016-08-08 21:55:40 +0900

    feat(env): implement app:env:debug

--- view/index.js
+++ view/index.js
@@ -3,6 +3,7 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 
 import './index.css'
+import env from './lib/env'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
@@ -15,3 +16,5 @@ const router   = new VueRouter({
 
 router.map(require('./configs/routes'))
 router.start(RootView, '.eg-root')
+
+console.log(env)
