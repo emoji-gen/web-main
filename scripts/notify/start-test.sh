efac03c90a055ad64cdfa25478a49ee27dc093ae
Pine Mizune <pinemz@gmail.com>
2016-08-08 21:23:08 +0900

    fix(scripts): add ci link

--- scripts/notify/start-test.sh
+++ scripts/notify/start-test.sh
@@ -5,7 +5,7 @@ ICON_URL="https://dl.dropboxusercontent.com/u/29279948/Slack/Icons/CircleCI.png"
 curl -XPOST \
     -d "token=$SLACK_API_TOKEN" \
     -d "channel=emoji" \
-    -d "text=start   test   \`Emoji-Web\` *$CIRCLE_BRANCH* by CircleCI" \
+    -d "text=start   test   \`Emoji-Web\` *$CIRCLE_BRANCH* <$CIRCLE_BUILD_URL|#$CIRCLE_BUILD_NUM>" \
     -d "username=CircleCI" \
     -d "icon_url=$ICON_URL" \
     "https://slack.com/api/chat.postMessage" > /dev/null 2>&1
