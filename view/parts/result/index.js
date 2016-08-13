84ea971da3f85abf869ab05358574d14ef221b2e
Pine Mizune <pinemz@gmail.com>
2016-08-13 17:33:33 +0900

    fix(result): remove official buttons

--- view/parts/result/index.js
+++ view/parts/result/index.js
@@ -5,8 +5,4 @@ import './index.css'
 module.exports = {
   name: 'eg-result',
   template: require('./index.html'),
-  components: {
-    'eg-facebook': require('../../components/facebook'),
-    'eg-hatena': require('../../components/hatena'),
-  },
 }
