2cb0cc12a96fa86e25419c597e2daa72cd2b3483
Pine Mizune <pinemz@gmail.com>
2016-08-13 18:47:44 +0900

    feat(result): add sns icons

--- view/parts/result/index.js
+++ view/parts/result/index.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-
 import './index.css'
 
 module.exports = {
   name: 'eg-result',
   template: require('./index.html'),
+  data: () => ({
+    visibleShare: false,
+  }),
+
+  methods: {
+    toggleShare: function () {
+      this.visibleShare = !this.visibleShare;
+    },
+  },
+
+  directives: {
+    'eg-sharer': require('../../directives/sharer'),
+  },
 }
