df1d4bdfae1108bb1c573f57b436319bb3704637
Pine Mizune <pinemz@gmail.com>
2016-08-13 20:42:37 +0900

    chore(static): npm run build

--- view/configs/routes.js
+++ view/configs/routes.js
@@ -4,6 +4,9 @@ module.exports = {
   '/': {
     component: require('../views/home'),
   },
+  '/emoji': {
+    component: require('../views/home'),
+  },
   '/contact': {
     component: require('../views/contact'),
   },
