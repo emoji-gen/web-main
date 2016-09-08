b1f7fd96e0d6c92a790b365bb4e9c812481b36a5
jiuya <tatsuya.yamamoto.kbhit@gmail.com>
2016-09-08 19:08:04 +0900

    fix(string2emoji.py): drow pixel slide & fix init image size

--- apps/String2emoji.py
+++ apps/String2emoji.py
@@ -27,7 +27,7 @@ class String2emoji(object):
         while self.getFont(hMax0).getsize(text)[1] > hMax :
             hMax0 = hMax0 - 1
 
-        for i in range(hMax0,hMax0*2):
+        for i in range(hMax0,hMax0*3):
             font = self.getFont(i)
             w, h = font.getsize(text)
             img = Image.new("RGBA",(w,h),self.backColor)
@@ -37,10 +37,10 @@ class String2emoji(object):
             y0 = self.stringOverBorderY(img,text,font,w)
             x1 = self.stringUnderBorderX(img,text,font,w,h) + x0
             y1 = self.stringUnderBorderY(img,text,font,h,w) + y0
-            if ((x1 >= wMax-5) and (x1 < wMax)) or ((y1 >= hMax-5) and (y1 < hMax)) :
-                return (i,x0,y0,x1,y1);
+            if ((x1 >= wMax-4) and (x1 < wMax)) or ((y1 >= hMax-2) and (y1 < hMax)) :
+                return (i,x0,y0,x1,y1)
     def stringOverBorderX(self,img,text,font,h):
-        for x in range(0,-127,-1):
+        for x in range(0,-255,-1):
             limitFlag = 0
             for i in range(0,h-1):
                 color = img.getpixel((-x,i))
@@ -49,7 +49,7 @@ class String2emoji(object):
             if limitFlag > 0:
                 return x
     def stringOverBorderY(self,img,text,font,w):
-        for y in range(0,-127,-1):
+        for y in range(0,-255,-1):
             limitFlag = 0
             for i in range(0,w-1,2):
                 color = img.getpixel((i,-y))
@@ -58,17 +58,17 @@ class String2emoji(object):
             if limitFlag > 0:
                 return y
     def stringUnderBorderX(self,img,text,font,w,h):
-        for cx in range(w-1,0,-2):
+        for cx in range(w-1,0,-1):
             for cy in range(0,h-1):
                 color = img.getpixel((cx,cy))
                 if color != self.backColor:
-                    return cx;
+                    return cx
     def stringUnderBorderY(self,img,text,font,h,w):
         for cy in range(h-1,0,-1):
             for cx in range(0,w-1,2):
                 color = img.getpixel((cx,cy))
                 if color != self.backColor:
-                    return cy;
+                    return cy
 
     def getEmoji(self):
         img = Image.new("RGBA",self.imageSize,self.backColor)
@@ -84,19 +84,19 @@ class String2emoji(object):
                 continue
             img_str = Image.new("RGBA",(len(self.textList[i])*128,128),self.backColor)
             draw = ImageDraw.Draw(img_str)
-            (size,x0,y0,x1,y1) = self.cutEffectiveRange(self.textList[i],len(self.textList[i])*64,int(128/l))
+            (size,x0,y0,x1,y1) = self.cutEffectiveRange(self.textList[i],len(self.textList[i])*127/l,int(127/l))
             #(size,x0,y0,x1,y1) = self.cutEffectiveRange(self.textList[i],256,128/l)
             font = self.getFont(size)
             draw.text((x0,y0), self.textList[i], fill=self.fontColor, font=font)
             img_str.crop((0,0,x1,y1))
-            if x1 > 128:
-                img_str = img_str.transform(img_str.size,Image.AFFINE,(x1/128.0,0,0,0,1,0),Image.BILINEAR)
+            if x1 > 127:
+                img_str = img_str.transform(img_str.size,Image.AFFINE,(x1/127.0,0,0,0,1,0),Image.BILINEAR)
                 image_paste_x = 0
             else:
-                image_paste_x = int((128-x1)/2)
+                image_paste_x = int((127-x1)/2)
                 #image_paste_x = 0
             if l != 1:
-                img.paste(img_str,(image_paste_x,int((128/l)*i)))
+                img.paste(img_str,(image_paste_x,int((127/l)*i)))
             else:
-                img.paste(img_str,(image_paste_x,int((128-y1)/2)))
+                img.paste(img_str,(image_paste_x,int((127-y1)/2)))
         return img
