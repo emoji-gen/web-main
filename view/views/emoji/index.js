5d10a365ef51af5e4c3689825fc9bfea991a557a
Pine Mizune <pinemz@gmail.com>
2016-12-04 00:59:58 +0900

    fix(view): support feature flag

--- view/views/emoji/index.js
+++ view/views/emoji/index.js
@@ -1,18 +1,26 @@
 import './index.css'
+import meta from '../../lib/meta'
 
 module.exports = {
   name: 'eg-emoji',
   template: require('./index.html'),
-  components: {
-    'eg-generator': require('../../parts/generator'),
-    'eg-recently': require('../../parts/recently'),
-    'eg-result': require('../../parts/result'),
+
+  data() {
+    return {
+      historyEnabled: meta.feature.history,
+    }
   },
+
   route: {
     activate: function () {
       if (~this.$route.path.indexOf('/emoji')) {
         this.$dispatch('EG_EMOJI_GENERATE', this.$route.query)
       }
     },
-  }
+  },
+  components: {
+    'eg-generator': require('../../parts/generator'),
+    'eg-recently': require('../../parts/recently'),
+    'eg-result': require('../../parts/result'),
+  },
 }
