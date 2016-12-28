2b2842d4b93576935fe0853bf5734b70eab9b879
Pine Mizune <pinemz@gmail.com>
2016-12-28 19:37:47 +0900

    fix(db): stretch default is true

--- db/migrations/20161228_size_fixed_align_stretch.sql
+++ db/migrations/20161228_size_fixed_align_stretch.sql
@@ -4,4 +4,4 @@
 ALTER TABLE `emoji_log`
   ADD `size_fixed` INTEGER UNSIGNED NOT NULL DEFAULT 0 AFTER `font`,
   ADD `align` VARCHAR(255) NOT NULL DEFAULT 'center' AFTER `size_fixed`,
-  ADD `stretch` INTEGER UNSIGNED NOT NULL DEFAULT 0 AFTER `align`;
+  ADD `stretch` INTEGER UNSIGNED NOT NULL DEFAULT 1 AFTER `align`;
