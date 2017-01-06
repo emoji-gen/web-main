cbae3e0e3ce7a10a57f6abeece8a7e827a9a2605
Pine Mizune <pinemz@gmail.com>
2017-01-06 17:14:49 +0900

    fix(git-status.sh): -z -> -n

--- scripts/test/git-status.sh
+++ scripts/test/git-status.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-if [ -z "$(git diff --raw)" ]; then
+if [ -n "$(git diff --raw)" ]; then
   echo "git repository is dirty"
   git diff --raw
   exit 1
 fi
 
-if [ -z "$(git diff --cached --raw)" ]; then
+if [ -n "$(git diff --cached --raw)" ]; then
   echo "git repository is dirty"
   git diff --cached --raw
   exit 1
