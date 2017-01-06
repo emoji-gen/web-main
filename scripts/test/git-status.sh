510f8230caa7d859fa6aaaa0def9c48c2cae8b80
Pine Mizune <pinemz@gmail.com>
2017-01-06 17:11:59 +0900

    fix(git-status): show debug data

--- scripts/test/git-status.sh
+++ scripts/test/git-status.sh
@@ -4,10 +4,12 @@ set -eu
 
 if [ -z "$(git diff --raw)" ]; then
   echo "git repository is dirty"
+  git diff --raw
   exit 1
 fi
 
 if [ -z "$(git diff --cached --raw)" ]; then
   echo "git repository is dirty"
+  git diff --cached --raw
   exit 1
 fi
