be42604fe3943a304cda9444d33e93343bd3d875
Pine Mizune <pinemz@gmail.com>
2018-04-02 22:40:23 +0900

    fix(db): use conf

--- src/emoji/db/__init__.py
+++ src/emoji/db/__init__.py
@@ -6,10 +6,10 @@ from aiomysql.sa import create_engine
 async def init_db(app):
     app['db'] = await create_engine(
         loop=app.loop,
-        user='root',
-        db='emoji',
-        host='127.0.0.1',
-        password='',
+        user=app['config']['mysql']['user'],
+        db=app['config']['mysql']['db'],
+        host=app['config']['mysql']['host'],
+        password=app['config']['mysql']['password'],
         charset='utf8mb4',
         autocommit=True
     )
