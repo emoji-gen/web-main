312eafbdecdbe37cb555d7b8681b0dbef2dfba5f
Pine Mizune <pinemz@gmail.com>
2018-12-26 11:47:00 +0900

    fix(ga): set content group

--- frontend/view/lib/ga/snippet.js
+++ frontend/view/lib/ga/snippet.js
@@ -5,3 +5,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-82501423-1', 'auto');
 ga('send', 'pageview');
+ga('set', 'contentGroup1', 'home');
