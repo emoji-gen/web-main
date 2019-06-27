b8ddde8d2a9f4617615a18a117eb01b97c88ffa6
Pine Mizune <pinemz@gmail.com>
2019-06-27 23:33:51 +0900

    feat: update generator

--- frontend/src/components/organisms/Generator.vue
+++ frontend/src/components/organisms/Generator.vue
@@ -18,6 +18,39 @@
 
     <!-- Parameters -->
     <div class="parameters">
+      <!-- Parameter : Text -->
+      <div class="parameter text">
+        <h3>テキスト</h3>
+        <textarea rows="2" cols="10"></textarea>
+
+        <h4>文字揃え</h4>
+        <div class="aligns">
+          <span class="align left">
+            <input type="radio" name="Generator__align" id="Generator__align--left" value="left">
+            <label for="Generator__align--left">左</label>
+          </span>
+          <span class="align center">
+            <input type="radio" name="Generator__align" id="Generator__align--center" value="center">
+            <label for="Generator__align--center">中央</label>
+          </span>
+          <span class="align right">
+            <input type="radio" name="Generator__align" id="Generator__align--right" value="right">
+            <label for="Generator__align--right">右</label>
+          </span>
+        </div>
+
+        <h4>サイズ調整</h4>
+        <div class="sizes">
+          <label>
+            <input type="checkbox">
+            文字サイズを固定する
+          </label>
+          <label>
+            <input type="checkbox">
+            自動で伸縮しない
+          </label>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,6 +59,11 @@
 <style lang="scss" scoped>
   @import 'includes/_variables';
   @import 'includes/_mixins';
+
+  %_parameter {
+    box-shadow: 0 0 1px 1px rgba(0, 0, 0, .15);
+    border-radius: 2px;
+  }
 
   .Generator {
     @extend %box;
@@ -40,6 +78,9 @@
       @extend %box-heading;
     }
 
+    /**
+     * Execution
+     */
     .execution {
       margin: 40px 0 0 0;
       text-align: center;
@@ -77,6 +118,127 @@
       }
     }
 
+    /*
+     * Parameters
+     */
+    .parameters {
+      display: flex;
+      justify-content: center;
+      margin: 38px 0 0;
+
+      .parameter {
+        margin: 0 30px 0;
+        text-align: center;
+
+        h3 {
+          color: $color-blue-dark;
+        }
+        h4 {
+          color: $color-gray;
+        }
+
+        /**
+         * Parameter : Text
+         */
+        &.text {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+
+          textarea {
+            @extend %_parameter;
+            margin: 0 0 24px 0;
+            padding: 10px;
+            border: 0;
+            width: 140px;
+            height: 100px;
+            text-align: center;
+            -webkit-appearance: none;
+          }
+
+          /**
+           * Parameter : Text : Align
+           */
+          .aligns {
+            display: flex;
+            justify-content: space-between;
+            width: 98px;
+            margin: -1px 0 24px;
+
+            .align {
+              $_size: 20px;
+              display: inline-block;
+              position: relative;
+              width: $_size;
+              height: $_size;
+              label {
+                position: absolute;
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                overflow: hidden;
+                padding-top: $_size;
+                background-size: $_size, 0;
+                background-repeat: no-repeat;
+                cursor: pointer;
+              }
+              input {
+                display: none;
+              }
+              input:checked + label {
+                background-size: 0, $_size;
+              }
+              &.left {
+                label {
+                  background-image: url('/assets/img/align_left.png'), url('/assets/img/align_left_active.png');
+                }
+              }
+              &.center {
+                label {
+                  background-image: url('/assets/img/align_center.png'), url('/assets/img/align_center_active.png');
+                }
+              }
+              &.right {
+                label {
+                  background-image: url('/assets/img/align_right.png'), url('/assets/img/align_right_active.png');
+                }
+              }
+            }
+          }
+
+          /**
+           * Parameter : Text : Size
+           */
+          .sizes {
+            display: flex;
+            flex-direction: column;
+            line-height: 1;
+            color: $color-gray;
+            text-align: left;
+            font-size: $font-xsmall;
+
+            label {
+              display: block;
+              cursor: pointer;
+
+              &:first-of-type {
+                margin-bottom: 6px;
+              }
+              input {
+                margin-right: 2px;
+              }
+            }
+          }
+        }
+
+        /**
+         * Parameter : Font
+         */
+
+
+      }
+    }
   }
 
 </style>
