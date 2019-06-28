a259ef3e0f1041be36148fda6bb2cd740f7ee6a0
Pine Mizune <pinemz@gmail.com>
2019-06-28 17:33:59 +0900

    feat: use eventbus

--- frontend/src/components/pages/App.vue
+++ frontend/src/components/pages/App.vue
@@ -17,7 +17,7 @@
 
 <script>
   import log from 'loglevel'
-  // import eventbus from '@/eventbus'
+  import eventbus from '@/eventbus'
 
   export default {
     created() {
@@ -26,11 +26,11 @@
       })
       this.$ptero.on('CE_SEARCH_JOINED_TEAMS_DONE', e => {
         log.debug('CE_SEARCH_JOINED_TEAMS_DONE', e.detail)
-        //this.$broadcast('CE_SEARCH_JOINED_TEAMS_DONE', e.detail)
+        eventbus.$emit('CE_SEARCH_JOINED_TEAMS_DONE', e.detail)
       })
       this.$ptero.on('CE_REGISTER_EMOJI_DONE', e => {
         log.debug('CE_REGISTER_EMOJI_DONE', e.detail)
-        //this.$broadcast('CE_REGISTER_EMOJI_DONE', e.detail)
+        eventbus.$emit('CE_REGISTER_EMOJI_DONE', e.detail)
       })
     },
   }
