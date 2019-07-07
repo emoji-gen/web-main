1e2fab1ed09b4dab1bbced8c443073d927d14627
Pine Mizune <pinemz@gmail.com>
2019-07-08 01:27:57 +0900

    feat: add `/ko`

--- server/src/emoji/routes/__init__.py
+++ server/src/emoji/routes/__init__.py
@@ -6,13 +6,19 @@ from emoji.routes import static, redirect
 from emoji.routes.emoji import download, generate
 from emoji.routes.api import font
 from emoji.routes.healthcheck import ok
-from emoji.routes.views import contact, index
+from emoji.routes import views
 
 
 def setup_routes(app):
-    app.router.add_get('/', index)
-    app.router.add_get('/result', index)
-    app.router.add_get('/contact', contact)
+    # Views : ja
+    app.router.add_get('/', views.index_ja)
+    app.router.add_get('/result', views.index_ja)
+    app.router.add_get('/contact', views.contact_ja)
+
+    # Views :ko
+    app.router.add_get('/ko', views.redirect_index_ko)
+    app.router.add_get('/ko/', views.index_ko)
+
     app.router.add_get('/healthcheck', ok)
     app.router.add_get('/emoji', generate)
     app.router.add_get('/emoji_download', download)
--- server/src/emoji/routes/views.py
+++ server/src/emoji/routes/views.py
@@ -3,14 +3,31 @@
 import json
 
 import aiohttp_jinja2
+from aiohttp.web_exceptions import HTTPMovedPermanently
 
 from emoji.repositories import emoji_log_repository
 
-async def index(request):
-    return await _render(request, 'home.html')
 
-async def contact(request):
-    return await _render(request, 'contact.html')
+# Views : ja
+#~~~~~~~~~~~~~~~~~~
+
+async def index_ja(request):
+    return await _render(request, 'home.html', locale='ja')
+
+async def contact_ja(request):
+    return await _render(request, 'contact.html', locale='ja')
+
+
+# Views : ko
+#~~~~~~~~~~~~~~~~~~
+
+async def redirect_index_ko(request):
+    return HTTPMovedPermanently('/ko/', headers={
+        'Cache-Control': 'public, immutable, max-age=31536000', # 1 year
+    })
+
+async def index_ko(request):
+    return await _render(request, 'home.html', locale='ko')
 
 
 async def _fetch_emoji_logs(emoji_service):
@@ -31,13 +48,17 @@ async def _fetch_emoji_logs(emoji_service):
     return emoji_logs
 
 
-async def _render(request, template):
+async def _render(request, template, *, locale='ja'):
     fonts = json.dumps(request.app['repos']['font'].all())
 
     emoji_service = request.app['services']['emoji']
     emoji_logs = json.dumps(await _fetch_emoji_logs(emoji_service))
 
-    response = aiohttp_jinja2.render_template(template, request, { 'fonts': fonts, 'emoji_logs': emoji_logs })
+    response = aiohttp_jinja2.render_template(template, request, {
+        'fonts': fonts,
+        'emoji_logs': emoji_logs,
+        'locale': locale,
+    })
     if request.app.debug:
         response.headers['Cache-Control'] = 'private, no-store, no-cache, must-revalidate'
     else:
