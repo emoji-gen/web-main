0230b06a33fcb6e52f4779345d076726b5093492
Pine Mizune <pinemz@gmail.com>
2019-07-07 16:39:12 +0900

    fix: fix locale support

--- server/src/locales.py
+++ server/src/locales.py
@@ -8,14 +8,16 @@ logger = logging.getLogger(__name__)
 class Locales():
     def __init__(self, config):
         self._locales = config.locales
-        self._locales_path = config.locales_path
+        self._locales_path = Path(config.locales_path).resolve()
+        self._messages = {}
 
     async def startup(self):
         for locale in self._locales:
-            locale_path = Path(self._locales_path).resolve().joinpath(locale + ".yml")
+            locale_path = self._locales_path.joinpath(locale + ".yml")
             if not locale_path.is_file():
                 raise RuntimeError(
-                    'A locale file is not found. :: locale={}, path={}'.format(locale, locale_path))
+                    'A locale file is not found. :: locale={}, path={}'.
+                        format(locale, locale_path))
 
     def get_message(key):
         pass
