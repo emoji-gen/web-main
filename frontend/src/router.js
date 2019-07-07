d6b5df46b1d69fe9631f568d12a8243662bd902b
Pine Mizune <pinemz@gmail.com>
2019-07-07 17:53:27 +0900

    feat: migrate new url

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -24,8 +24,29 @@ const routes = [
 // -------------------------------------------------------------------
 
 export default () => {
-  return new VueRouter({
+  const router = new VueRouter({
     mode: 'history',
     routes,
   })
+
+  router.beforeEach((to, from, next) => {
+    // Migrate hash mode to history mode.
+    // ref. https://github.com/emoji-gen/web-main/blob/14dc13707e40584becdbd2f73752209891e1215a/frontend/view/configs/routes.js
+    if (to.fullPath === '/#!/') {
+      next({ path: '/' })
+      return
+    }
+    if (to.fullPath === '/#!/contact') {
+      next({ path: '/contact' })
+      return
+    }
+    if (to.fullPath.startsWith('/#!/emoji')) {
+      next({ path: to.fullPath.replace(/\/\#\!\/emoji/, '/result') })
+      return
+    }
+
+    next()
+  })
+
+  return router
 }
