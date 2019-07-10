afce0a14c7abac7e8ebbdc5a4c929c7599b81f0d
Pine Mizune <pinemz@gmail.com>
2019-07-11 01:59:33 +0900

    fix: fix lint err

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,10 +10,10 @@ import log from 'loglevel'
 import 'normalize.css'
 import './style.scss'
 
-import { App } from './components'
-import setupRouter from './router'
-import { messages  } from './locales'
-import { LOCALE  } from './initial_state'
+import { App } from '@/src/components'
+import { LOCALE } from '@/src/initial_state'
+import { messages } from '@/src/locales'
+import setupRouter from '@/src/router'
 
 // -------------------------------------------------------------------
 
@@ -27,12 +27,12 @@ Vue.use(VuePtero, { target: document.body })
 Vue.use(VueRouter)
 Vue.use(VueI18n)
 
-// -------------------------------------------------------------------
-
 const i18n = new VueI18n({
   locale: LOCALE,
   messages,
 })
+
+// -------------------------------------------------------------------
 
 export default new Vue({
   el: '.App',
